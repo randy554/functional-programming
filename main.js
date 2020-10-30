76acd60877fe1c7f4fb2cd778e9485339b11800d
randy554 <randy554@hotmail.com>
2020-10-30 12:15:45 +0100

    refactor foreach

--- main.js
+++ main.js
@@ -1,39 +1,64 @@
-console.log("JS is alive!");
+// console.log("JS is alive!");
 
 // Get dataset & assign to variable
-const stuData   = data;
+const studentenData   = data;
 
 // console.log("Juiste Hele dataset: ", stuData);
 // console.log("Eerste dataset2: ", stuData[0]);
 
 // If any empty input, set it 0 en return the new list
-let fullyFilledList     = emptyInputToIntegers(stuData);
+let fullyFilledList     = emptyInputToIntegers(studentenData);
 
 // Set all string values to integers
+// let allIntegersList     = stringToIntegers(fullyFilledList);
 let allIntegersList     = stringToIntegers(fullyFilledList);
 
 // Check for strange values & append them to the list
 // getSkepticalInput(allIntegersList);
-filterGetSkepticalInput(allIntegersList);
+getSkepticalInput(allIntegersList);
 
 
 
     //  Detect & remove empty fields & return a new dataList
+    // function emptyInputToIntegers(data) {
+    //
+    //     let newDataList = [];
+    //     let giveInteger = 0;
+    //
+    //     data.forEach(function (value) {
+    //
+    //         if(value.schermtijdPerDagVolgensTelefoon.length === 0) {
+    //
+    //             newDataList.push(giveInteger);
+    //
+    //         }else {
+    //
+    //             newDataList.push(value.schermtijdPerDagVolgensTelefoon);
+    //         }
+    //
+    //     });
+    //
+    //     console.log("No empty input fields list: ", newDataList);
+    //     return newDataList;
+    //
+    // }
+
+    //  Detect & remove empty fields & return a new dataList
     function emptyInputToIntegers(data) {
 
-        let newDataList = [];
-        let giveInteger = 0;
 
-        data.forEach(function (value) {
+        const giveInteger = 0;
+
+        let newDataList =   data.map(function (value) {
 
             if(value.schermtijdPerDagVolgensTelefoon.length === 0) {
 
-                console.log(value.schermtijdPerDagVolgensTelefoon.length);
-                newDataList.push(giveInteger);
+                return giveInteger;
 
             }else {
 
-                newDataList.push(value.schermtijdPerDagVolgensTelefoon);
+                return value.schermtijdPerDagVolgensTelefoon;
+
             }
 
         });
@@ -44,32 +69,33 @@ filterGetSkepticalInput(allIntegersList);
     }
 
     //  Detect skeptical input & return a new dataList
+    // function getSkepticalInput(data) {
+    //
+    //     let newDataList     = [];
+    //     let skepticalList   = [];
+    //
+    //     data.forEach(function (value){
+    //
+    //         if(value <= 10 || value >= 1200){
+    //             console.log("NIET GELDIG!");
+    //
+    //             skepticalList.push(value);
+    //
+    //         }
+    //
+    //         newDataList.push(value);
+    //
+    //     });
+    //
+    //     newDataList.push(skepticalList);
+    //
+    //     console.log("Full list with potential skeptical data: ", newDataList);
+    //     return newDataList;
+    //
+    // }
+
+    //  Detect skeptical input & return a new dataList
     function getSkepticalInput(data) {
-
-        let newDataList     = [];
-        let skepticalList   = [];
-
-        data.forEach(function (value){
-
-            if(value <= 10 || value >= 1200){
-                console.log("NIET GELDIG!");
-
-                skepticalList.push(value);
-
-            }
-
-            newDataList.push(value);
-
-        });
-
-        newDataList.push(skepticalList);
-
-        console.log("Full list with potential skeptical data: ", newDataList);
-        return newDataList;
-
-    }
-
-    function filterGetSkepticalInput(data) {
 
         let filteredList    = data.filter(function (minuten) {
 
@@ -77,22 +103,39 @@ filterGetSkepticalInput(allIntegersList);
 
         });
 
-        console.log("ja: ", filteredList);
+        data.push(filteredList);
+
+        console.log("Full list with potential skeptical data: ", data);
+        return data;
 
     }
 
     //  Convert string input to integers & return a new dataList
+    // function stringToIntegers(data) {
+    //
+    //     let newDataList = [];
+    //
+    //     data.forEach(function (value) {
+    //
+    //         let newInteger = parseInt(value);
+    //         newDataList.push(newInteger);
+    //
+    //     });
+    //
+    //     console.log("All input fields stringToInteger list: ", newDataList);
+    //     return newDataList;
+    //
+    // }
+
+    //  Convert string input to integers & return a new dataList
     function stringToIntegers(data) {
 
-        let newDataList = [];
+        let newDataList = data.map(function (value){
 
-        data.forEach(function (value) {
-
-            let newInteger = parseInt(value);
-            newDataList.push(newInteger);
+            return parseInt(value);
 
         });
-
+        
         console.log("All input fields stringToInteger list: ", newDataList);
         return newDataList;
 
